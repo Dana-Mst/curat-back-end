82909870a41e097445fb9bdce3aef759d51693a6
danamusetescu <danactt27@gmail.com>
2021-10-13 11:59:03 +0300

    CrossOrigin annotation

--- src/main/java/com/curat/eshopbackend/category/CategoryController.java
+++ src/main/java/com/curat/eshopbackend/category/CategoryController.java
@@ -1,6 +1,7 @@
 package com.curat.eshopbackend.category;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/category")
 public class CategoryController {
 
